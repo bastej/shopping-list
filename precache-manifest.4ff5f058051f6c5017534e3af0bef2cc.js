b0c61668e89e692728187d29e228b95df25b9038
DESKTOP-G2H22OC\SSiatkaPA <s.siatka@promoagency.pl>
2019-01-11 14:06:15 +0100

    Updates

---
+++ precache-manifest.4ff5f058051f6c5017534e3af0bef2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c5c20142f6b05843f06",
+    "url": "/static/js/main.9c5c2014.chunk.js"
+  },
+  {
+    "revision": "fc9e4fdd24dfaf3f50bb",
+    "url": "/static/js/1.fc9e4fdd.chunk.js"
+  },
+  {
+    "revision": "9c5c20142f6b05843f06",
+    "url": "/static/css/main.d918f0e9.chunk.css"
+  },
+  {
+    "revision": "fc9e4fdd24dfaf3f50bb",
+    "url": "/static/css/1.950088ce.chunk.css"
+  },
+  {
+    "revision": "66e04f761815d16cbdceab8c9bd726e3",
+    "url": "/index.html"
+  }
+];
