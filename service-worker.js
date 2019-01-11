075a0c03ab09eccb7ebdb18f89a70699caaf991e
DESKTOP-G2H22OC\SSiatkaPA <s.siatka@promoagency.pl>
2019-01-11 14:13:56 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ff5f058051f6c5017534e3af0bef2cc.js"
+  "/shopping-list/precache-manifest.ff25bdacb5de8b31e5c2553c91158721.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/shopping-list/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
